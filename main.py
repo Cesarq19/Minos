5db5506d0a0c936e2303430053709af0b8e00108
Cesarq19 <cepaquin@espol.edu.ec>
2023-04-21 03:03:43 -0500

    Actualizacion

--- main.py
+++ main.py
@@ -1,33 +1,23 @@
-from hcsr04 import SR04
-from machine import Pin
-import time
+from machine import Pin, PWM
+from time import sleep_ms
+from stepper import DCMotor
 
-# Define the ultrasonic sensors
-SR_LEFT = SR04()
-SR_RIGHT = SR04()
-SR_FORWARD = SR04()
+frequency = 15000 
+IN1 = Pin(0, Pin.OUT)
+IN2 = Pin(1, Pin.OUT)
+enable1 = PWM(Pin(4), frequency)
 
+IN3 = Pin(2, Pin.OUT)
+IN4 = Pin(3, Pin.OUT)
+enable2 = PWM(Pin(5),frequency)
 
-def getSonar():
-    trigPin.value(1)
-    time.sleep_us(10)
-    trigPin.value(0)
-    
-    while not echoPin.value():
-        pass
-    
-    pingStart = time.ticks_us()
-    
-    while echoPin.value():
-        pass
-    
-    pingStop = time.ticks_us()
-    pingTime = time.ticks_diff(pingStop, pingStart)
-    distance = pingTime*soundVelocity//2//10000
-    return int(distance)
-
-time.sleep_ms(2000)
+motorA = DCMotor(IN1, IN2, enable1) # Configurar motor A con los pines 14 y 27
+motorB = DCMotor(IN3, IN4, enable2) # Configurar motor B con los pines 26 y 25
 
 while True:
-    time.sleep_ms(500)
-    print('Distance: ', getSonar(), ' cm')
+    motorB.forward(100)
+    motorA.forward(100)
+    sleep_ms(2000)
+    motorB.backwards(100)
+    motorA.backwards(100)
+    sleep_ms(2000)
