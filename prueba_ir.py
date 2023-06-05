a71a38ab179e471fb2cecb2a49b3ddec73339db7
kevin santana <kevin_santana120@hotmail.com>
2023-06-05 14:06:03 -0500

    added  functions

--- prueba_ir.py
+++ prueba_ir.py
@@ -1,6 +1,23 @@
+# Import necessary libraries
 from machine import Pin
-from time import sleep_ms
-sensor_ir=Pin(2,Pin.IN)
-while True :
-    print(sensor_ir.value())
-    sleep_ms(500)
+import time
+
+# Define infrared sensor pins
+LEFT_SENSOR_PIN = 12
+FRONT_SENSOR_PIN = 14
+RIGHT_SENSOR_PIN = 27
+LEFT_45_SENSOR_PIN = 33
+RIGHT_45_SENSOR_PIN = 32
+
+# Initialize infrared sensor pins as input pins
+left_sensor = Pin(LEFT_SENSOR_PIN, Pin.IN)
+front_sensor = Pin(FRONT_SENSOR_PIN, Pin.IN)
+right_sensor = Pin(RIGHT_SENSOR_PIN, Pin.IN)
+left_45_sensor = Pin(LEFT_45_SENSOR_PIN, Pin.IN)
+right_45_sensor = Pin(RIGHT_45_SENSOR_PIN, Pin.IN)
+def sensar_paredes(direccion, position):
+    left_value = left_sensor.value()
+    front_value = front_sensor.value()
+    right_value = right_sensor.value()
+    return left_value,front_value,right_value
+
