fe005a8a8c9d716e361186c75994934278220226
kevin santana <kevin_santana120@hotmail.com>
2023-06-13 21:46:44 -0500

    realize la funcion para determinar la siguiente posicion del carro

--- posicion_actual.py
+++ posicion_actual.py
@@ -1,6 +1,6 @@
 
 position =(4,0)#tengo la position del carro
-direccion="norte"#obtengo la direccion del carro 
+direccion=0 #obtengo la direccion del carro 
 recorrido=[]#donde #se almacena el recorrido 
 recorrido.append(position)#almaceno la posiicion
 flood_fill(4,4)#genero los pesos
@@ -8,6 +8,7 @@ matriz_pesos=maze_weights
 frente,derecha,izquierda=sensar_paredes()
 def actualizar_paredes():
     """esta funcion verifica las paredes y las almacena en las matrices de paredes"""
+
     if not (frente and derecha and izquierda):#si no encuentra ninguna pared no hace nada 
         pass
     elif position=="sur"and (frente or derecha or izquierda):
@@ -47,4 +48,26 @@ def actualizar_paredes():
         if derecha and izquierda:
             maze_hwalls[position[0],position[1]]=3
 
-def 
+def siguiente_posicion():
+    """"la funcion obtiene la siguiente posicion y la orientacion a la que debe esta el carro """
+    avances=[]
+    orientacion_siguiete=direccion
+    celda_siguiente=(0,0)
+    #dependiendo de la direccion seleccionamos el sentido que vamos a verificar las celdas 
+    if direccion==0:
+        avances=[(-1,0),(0,1),(1,0),(0,-1)]
+    elif direccion==90:
+        avances=[(0,1),(1,0),(0,-1),(-1,0)]
+    elif direccion==180:
+        avances=[(1,0),(0,-1),(-1,0),(0,1)]
+    elif direccion==270:
+        avances=[(0,-1),(-1,0),(0,1),(1,0)]
+    for avance in avances:
+        nx,ny=position[0]+avance[0],position[1]+avance[1]
+        orientacion_siguiete+=90
+        if orientacion_siguiete>270:
+            orientacion_siguiete=0
+        if matriz_pesos[nx][ny]==(matriz_pesos[nx][ny]+1):
+            celda_siguiente=(nx,ny)
+            break
+    return celda_siguiente,orientacion_siguiete
