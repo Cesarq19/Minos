56c695f615ad76e6c03bab8d2a396dfefa88fbb8
kevin santana <kevin_santana120@hotmail.com>
2023-06-15 14:19:02 -0500

    calibrado el script para probar los infrarojos

--- prueba_irs.py
+++ prueba_irs.py
@@ -1,28 +1,28 @@
 # Import necessary libraries
 from machine import Pin
 import time
-
+from time import sleep_ms   
 # Define infrared sensor pins
-LEFT_SENSOR_PIN = 12
-FRONT_SENSOR_PIN = 14
-RIGHT_SENSOR_PIN = 27
-LEFT_45_SENSOR_PIN = 33
-RIGHT_45_SENSOR_PIN = 32
+LEFT_SENSOR_PIN = 33
+FRONT_SENSOR_PIN = 34
+RIGHT_SENSOR_PIN = 35
+# LEFT_45_SENSOR_PIN = 33
+# RIGHT_45_SENSOR_PIN = 32
 
 # Initialize infrared sensor pins as input pins
 left_sensor = Pin(LEFT_SENSOR_PIN, Pin.IN)
 front_sensor = Pin(FRONT_SENSOR_PIN, Pin.IN)
 right_sensor = Pin(RIGHT_SENSOR_PIN, Pin.IN)
-left_45_sensor = Pin(LEFT_45_SENSOR_PIN, Pin.IN)
-right_45_sensor = Pin(RIGHT_45_SENSOR_PIN, Pin.IN)
+# left_45_sensor = Pin(LEFT_45_SENSOR_PIN, Pin.IN)
+# right_45_sensor = Pin(RIGHT_45_SENSOR_PIN, Pin.IN)
 
 # Define function to check if a cell is accessible
 def is_accessible():
     left_value = left_sensor.value()
     front_value = front_sensor.value()
     right_value = right_sensor.value()
-    left_45_value = left_45_sensor.value()
-    right_45_value = right_45_sensor.value()
+    # left_45_value = left_45_sensor.value()
+    # right_45_value = right_45_sensor.value()
 
     # Check if there is a wall or obstacle in the way
     if left_value and front_value and right_value:
@@ -35,8 +35,13 @@ def is_accessible():
     # Otherwise, the cell is accessible
     return True
 
-# Example usage
-if is_accessible():
-    print("Cell is accessible")
-else:
-    print("Cell is blocked")
+# # Example usage
+# if is_accessible():
+#     print("Cell is accessible")
+# else:
+#     print("Cell is blocked")
+while True :
+    print(front_sensor.value())
+    print(left_sensor.value())
+    print(right_sensor.value())
+    sleep_ms(500)
